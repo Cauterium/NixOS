591b366cb43badb98874e61956845305221a7145
Fabian Brenneisen <brenneisen.fabian@gmail.com>
2024-06-21 18:40:36 +0200

    feat(spicetify): change theme to comfy

--- homeManagerModules/spicetify.nix
+++ homeManagerModules/spicetify.nix
@@ -6,6 +6,11 @@
   ...
 }: let
   spicetifyPkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
+  comfySrc = pkgs.fetchgit {
+    url = "https://github.com/Comfy-Themes/Spicetify";
+    rev = "8f79774155455b3c1beafb4c0863eeb684c23ea8";
+    sha256 = "sha256-Oel1vHHCAkVIlSnU4z1rSj84YHYspzUItAGO1jvxUrQ=";
+  };
 in {
   imports = [inputs.spicetify-nix.homeManagerModule];
 
@@ -16,28 +21,53 @@ in {
   config = lib.mkIf config.spicetify.enable {
     programs.spicetify = {
       enable = true;
-      theme = spicetifyPkgs.themes.Blossom;
+      theme = {
+        name = "Comfy";
+        src = comfySrc;
+        requiredExtensions = [
+          {
+            filename = "theme.js";
+            src = "${comfySrc}/Comfy";
+          }
+        ];
+        appendName = true;
+        injectCss = true;
+        replaceColors = true;
+        overwriteAssets = true;
+        sidebarConfig = false;
+      };
+
       colorScheme = "custom";
       customColorScheme = with config.colorScheme.colors; {
         text = "${base07}";
         subtext = "${base05}";
         nav-active-text = "${base0C}";
         main = "${base00}";
+        main-secondary = "${base01}";
+        main-elevated = "${base00}";
+        main-transition = "${base00}";
+        highlight = "${base03}";
+        highlight-elevated = "${base03}";
         sidebar = "${base01}";
         player = "${base01}";
         card = "${base01}";
         window = "${base01}";
-        main-secondary = "${base01}";
+        shadow = "${base01}";
         button = "${base0C}";
         button-secondary = "${base07}";
         button-active = "${base0C}";
         button-disabled = "${base07}";
         nav-active = "${base02}";
-        play-button = "${base07}";
         tab-active = "${base02}";
         notification = "${base03}";
         notification-error = "${base08}";
         playback-bar = "${base0C}";
+        play-button = "${base0C}";
+        play-button-active = "${base0C}";
+        progress-fg = "${base0C}";
+        progress-bg = "${base01}";
+        pagelink-active = "${base03}";
+        radio-btn-active = "${base03}";
         misc = "000000";
       };
 
