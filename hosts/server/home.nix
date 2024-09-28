f8f83a30c7cf68295ad5d21b1b7b5b92aa2ce615
Fabian Brenneisen <brenneisen.fabian@gmail.com>
2024-09-28 12:43:12 +0200

    feat(server): add home.nix for server

---
+++ hosts/server/home.nix
@@ -0,0 +1,63 @@
+{
+  inputs,
+  outputs,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./../../homeManagerModules
+  ];
+
+  desktopApps.enable = false;
+  development.enable = false;
+  hyprland.enable = false;
+  nix-colors.enable = false;
+  secrets.enable = false;
+  spicetify.enable = false;
+  theming.enable = false;
+  waybar.enable = false;
+
+  home.username = "cauterium";
+  home.homeDirectory = "/home/cauterium";
+
+  # Configure nix package manager
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home.stateVersion = "23.11"; # Please check Home Manager release notes before changing.
+
+  home.packages = with pkgs; [
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    jdk21
+    jq
+  ];
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+}
