d9dea136151fe7bcb470b87d7f9ce9d342ad81b4
Fabian Brenneisen <brenneisen.fabian@gmail.com>
2025-04-29 12:29:18 +0200

    feat(dev): remove packages for haskell

--- homeManagerModules/development.nix
+++ homeManagerModules/development.nix
@@ -11,7 +11,6 @@
 
   config = lib.mkIf config.development.enable {
     home.packages = with pkgs; [
-      ghc
       jetbrains.pycharm-professional
       python3
     ];
@@ -23,9 +22,7 @@
         arrterian.nix-env-selector
         enkia.tokyo-night
         github.copilot
-        haskell.haskell
         jnoortheen.nix-ide
-        justusadam.language-haskell
         llvm-vs-code-extensions.vscode-clangd
         rust-lang.rust-analyzer
         usernamehw.errorlens
