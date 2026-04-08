06fc005741aae0f52676a52be6ae8e7637027c89
Fabian Brenneisen <brenneisen.fabian@gmail.com>
2026-04-08 17:35:12 +0200

    feat(browser): add mods to zen-browser config

--- homeManagerModules/desktopApps.nix
+++ homeManagerModules/desktopApps.nix
@@ -118,6 +118,8 @@
 
     programs.zen-browser = let
       settings = {
+        "browser.bookmarks.restore_default_bookmarks" = false;
+
         "zen.glance.activation-method" = "ctrl";
         "zen.urlbar.behavior" = "float";
         "zen.view.compact.hide-toolbar" = true;
@@ -127,6 +129,17 @@
         "zen.workspaces.continue-where-left-off" = true;
         "zen.workspaces.separate-essentials" = false;
       };
+      mods = [
+        "906c6915-5677-48ff-9bfc-096a02a72379"  # Floating Status Bar
+        "664c54f9-d97d-410b-a479-23dd8a08a628"  # Better Tab Indicators
+        "cb15abdb-0514-4e09-8ce5-722cf1f4a20f"  # Hide Extension Name
+        "81fcd6b3-f014-4796-988f-6c3cb3874db8"  # Zen Context Menu
+        "d93e67f8-e5e1-401e-9b82-f9d5bab231e6"  # Super URL Bar
+        "c8d9e6e6-e702-4e15-8972-3596e57cf398"  # Zen Back Forward
+        "ad97bb70-0066-4e42-9b5f-173a5e42c6fc"  # Super Pins
+        "f7c71d9a-bce2-420f-ae44-a64bd92975ab"  # Better Unloaded Tabs
+        "1e86cf37-a127-4f24-b919-d265b5ce29a0"  # Lean
+      ];
     in {
       enable = true;
       policies = {
@@ -152,6 +165,7 @@
         id = 0;
         isDefault = config.desktopApps.zen-browser.defaultProfile == "cauterium";
         settings = settings;
+        mods = mods;
         extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
           bitwarden
           darkreader
@@ -166,6 +180,7 @@
         id = 1;
         isDefault = config.desktopApps.zen-browser.defaultProfile == "freetime";
         settings = settings;
+        mods = mods;
         extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
           bitwarden
           darkreader
