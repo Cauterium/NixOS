af6e8cf7a5b10175c099059cef0e1dd150bfe399
Fabian Brenneisen <brenneisen.fabian@gmail.com>
2026-04-20 08:45:59 +0200

    fix(home-manager): fix home-manager service failing

--- homeManagerModules/audio.nix
+++ homeManagerModules/audio.nix
@@ -10,8 +10,8 @@
   config = lib.mkIf config.audio.enable {
     services.easyeffects.enable = true;
 
-    systemd.user.services.easyeffects.environment = {
-      LSP_WS_LIB_GLXSURFACE = "off";
-    };
+    systemd.user.services.easyeffects.Service.Environment = [
+      "LSP_WS_LIB_GLXSURFACE=off"
+    ];
   };
 }
