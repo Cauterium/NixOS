0c84360e3542651707080d3205c36586ab8ce9be
Fabian Brenneisen <brenneisen.fabian@gmail.com>
2026-06-21 10:05:41 +0200

    feat(laptop): add nbfc to control laptop fan speed

--- nixosModules/laptop.nix
+++ nixosModules/laptop.nix
@@ -9,6 +9,20 @@
   };
 
   config = lib.mkIf config.laptop.enable {
+    environment.systemPackages = with pkgs; [
+      nbfc-linux
+    ];
+
+    systemd.services.nbfc_service = {
+      enable = true;
+      description = "NoteBook FanControl service";
+      serviceConfig.Type = "simple";
+      path = [pkgs.kmod];
+
+      script = "${pkgs.nbfc-linux}/bin/nbfc_service --config-file '/home/cauterium/.config/nbfc.json'";
+      wantedBy = ["multi-user.target"];
+    };
+
     services.thermald.enable = true;
     services.upower.enable = true;
     services.auto-cpufreq.enable = true;
